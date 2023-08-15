1c1a921654d1660fd92f01c6460e3d5bb86a077a
MaxMitin <maksim.mitin@mail.ru>
2023-08-15 16:58:09 +0300

    Corrected

--- src/main/java/com/example/newflowers/HelloController.java
+++ src/main/java/com/example/newflowers/HelloController.java
@@ -1,14 +1,66 @@
 package com.example.newflowers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Label first;
+
+    @FXML
+    Label second;
+
+    @FXML
+    Label third;
+
+    @FXML
+    Button masha;
+
+    @FXML
+    Button tanya;
+
+    @FXML
+    Button day;
+
+    public void actionOfMasha()
+    {
+        String s=second.getText();
+        second.setText(third.getText());
+        third.setText(s);
     }
+
+    public void actionOfTanya()
+    {
+        String s1 =second.getText();
+        second.setText(first.getText());
+        first.setText(s1);
+    }
+
+    public void actionOfDay()
+    {
+        String s=second.getText();
+        second.setText(third.getText());
+        third.setText(s);
+
+        String s1 =second.getText();
+        second.setText(first.getText());
+        first.setText(s1);
+    }
+
+
+
 }
+
+
+
+
+    //@FXML
+    //private Label welcomeText;
+
+    //@FXML
+    //protected void onHelloButtonClick() {
+        //welcomeText.setText("Welcome to JavaFX Application!");
+    //}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.newflowers {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.newflowers to javafx.fxml;
     exports com.example.newflowers;
 }
